924c3328d32577df761fc4c2f0f446270193d151
CubiZm <seo-xcube@yandex.ru>
2016-06-25 23:24:22 +0400

    add template for other functions

--- js/add.js
+++ js/add.js
@@ -1,10 +1,21 @@
 'use strict';
 
-define(function() {
- 
-  function getEdit() {
-    console.log('jopa');
+define(['./edit'], function(getEdit) {
+
+  function setAdd() {
+    var template = document.querySelector('#template');
+    var inputName = document.querySelector('.template-name');
+    var inputCount = document.querySelector('.template-count');
+    var inputPrice = document.querySelector('.template-price');
+
+    var nameValue = inputName.value;
+    var countValue = inputCount.value;
+    var priceValue = inputPrice.value;
+
+    console.log(nameValue);
+    var editBtn = document.querySelector('.edit');
+    editBtn.addEventListener('click', getEdit);
   }
 
-  return getEdit;
-})
+  return setAdd;
+});
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
-require(['./load', './add'], function(getLoad, getEdit) {
+require(['./load', './add'], function(getLoad, setAdd) {
   var loadBtn = document.querySelector('.load-btn');
   loadBtn.addEventListener('click', getLoad);
 
   var addBtn = document.querySelector('.add-btn');
-  addBtn.addEventListener('click', getEdit);
+  addBtn.addEventListener('click', setAdd);
+
+
 });
